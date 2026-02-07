426c8ce5820afbe2d6dc77cd516536a9277ce6eb
madhavgarg2597-tech <madhavgarg2597@gmail.com>
2026-02-08 01:57:54 +0530

    Clean repo: keep only core source files

--- cnn_training.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-import numpy as np   
-from tensorflow.keras.activations import linear, relu, sigmoid
